07a76400e37428cf8824e302843b746316ce93a0
Dan Siroky <dansiroky@gmail.com>
2017-12-15 20:32:47 +0000

    Messing around with Altair, Vega-Lite, and Vega

--- visVega.py
+++ visVega.py
@@ -5,7 +5,7 @@ from ladybug.analysisperiod import AnalysisPeriod
 import googlemaps
 import json
 import pandas as pd
-from altair import Row, Column, Chart, Text, Color, Scale, Bin, Axis
+from altair import Row, Column, Chart, Text, Color, Scale, Bin, Axis, Legend
 
 def f(x):
     x = x * 1.8 + 32
@@ -32,24 +32,48 @@ data['blanks'] = ""
 data['dt'] = pd.to_datetime(data['dt'], format="%d %b %H:%M")
 data['temp'] = data['temp'].astype('float64')
 data['temp'] = data['temp'].apply(f)
+json_filename = sys.argv[2] + '.json'
+data.to_json(path_or_buf=json_filename)
 
-colors = ["#67001f","#b2182b","#d6604d","#f4a582","#fddbc7","#d1e5f0","#92c5de","#4393c3","#2166ac","#053061"]
+colors =   ["#67001f",
+            "#b2182b",
+            "#d6604d",
+            "#f4a582",
+            "#fddbc7",
+            "#d1e5f0",
+            "#92c5de",
+            "#4393c3",
+            "#2166ac",
+            "#053061"]
+
 colors = colors[::-1]
+
+#d = [0, 12, 24, 36, 48, 60, 72, 84, 96, 108]
+d = [0, 120]
+r = Row('dt:T', timeUnit='hours', axis=Axis(title='Hour of day'))
+c = Column('dt:T', 
+           timeUnit='monthdate', 
+           axis=Axis(format=u'%b', labels=False, title='Month')
+        ) 
+col = Color('temp:N',
+            bin=Bin(step=12),
+            scale=Scale(domain=[0,120], range=colors, clamp=True, zero=True),
+            #scale=Scale(range=colors, domain=[0, 120], zero=True),
+            legend=Legend(title="Temperature", format=u'.0f')
+           )
+
 chart = Chart(data).mark_text(
-               applyColorToBackground=True,
-           ).encode(
-               row=Row('dt:T', timeUnit='hours', axis=Axis(title='Hour of day')),
-               column=Column('dt:T', timeUnit='monthdate', axis=Axis(
-            format=u'%b', labels=False, title='Month'
-)),
+                applyColorToBackground=True
+            ).encode(
+               row=r,
+               column=c,
                text=Text('blanks'),
-               color=Color('temp:N', bin=Bin(maxbins=10, step=5), clamp=True,
-                    scale=Scale(range=colors, domain=[0, 100]), axis=Axis(title='Temperature')
-                )
+               color=col
            ).configure_scale(
-               textBandWidth=2,
-               bandSize=15
+               textBandWidth=3,
+               bandSize=25
            )
            
 chart.max_rows = 8761
-chart.savechart('plot.html')
+filename = sys.argv[2] + ".html"
+chart.savechart(filename)

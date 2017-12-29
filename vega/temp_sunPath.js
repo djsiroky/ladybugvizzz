2e37ceb6156d6c5184fd1dd293b295932ec90fdb
Dan Siroky <dansiroky@gmail.com>
2017-12-29 17:21:39 -0600

    Additional helper functions for sunpath data creation

--- vega/temp_sunPath.js
+++ vega/temp_sunPath.js
@@ -18,3 +18,10 @@ hours.forEach(hour => { pos.push(solarCalculator([39.0839389, -94.5818676]).posi
 
 pos.forEach(item => { posObj.push({"size": 100, "lon": item[0], "lat": item[1]}) })
 
+
+
+
+var coords3 = d3.utcDays(start, end).map(function(day) {
+    return hours.map(hour => { return {"hour": hour, "month": (1 + day.getMonth()), "date": day.getDate(), "pos": solar.position(d3.utcHour.offset(day, hour))} })
+})
+flat = [].concat.apply([], coords3)
